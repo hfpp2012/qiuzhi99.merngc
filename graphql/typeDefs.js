9b4cad9d45c02b913cfd27f62b3500c716f69e79
hfpp2012 <hfpp2012@gmail.com>
2019-09-17 21:06:53 +0800

    check-auth createpost deletepost

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -10,6 +10,7 @@ module.exports = gql`
 
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
 
   type User {
@@ -30,5 +31,7 @@ module.exports = gql`
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
   }
 `;
