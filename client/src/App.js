20cdba777a1259ac3d6b9c47aa6aa569f9a97615
hfpp2012 <hfpp2012@gmail.com>
2019-10-22 15:50:17 +0800

    03_react_router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div>
-      <h1>hi world</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
