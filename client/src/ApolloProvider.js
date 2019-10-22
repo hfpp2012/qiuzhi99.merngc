c2741c195e8a7c154297f6240a286234c8867da1
hfpp2012 <hfpp2012@gmail.com>
2019-10-22 13:30:10 +0800

    02_apollo_client

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,19 @@
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const httpLink = createHttpLink({ uri: "http://localhost:5001" });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
