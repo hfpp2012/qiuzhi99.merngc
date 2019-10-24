498b7f93b7127b2049f5b0179fd3354bfdeb4935
hfpp2012 <hfpp2012@gmail.com>
2019-10-24 22:46:27 +0800

    05_useQuery

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import ApolloProvider from "./ApolloProvider";
 import * as serviceWorker from "./serviceWorker";
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,34 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 function Home() {
+  const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+
+  console.log(data);
+
   return (
     <div>
       <h1>Home page</h1>
