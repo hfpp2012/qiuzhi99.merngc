a2c10dab5338f90f387fadd54a289c274f4c7261
hfpp2012 <hfpp2012@gmail.com>
2019-11-25 22:43:57 +0800

    32_finished

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { setContext } from "apollo-link-context";
+import { onError } from "apollo-link-error";
 
 const httpLink = createHttpLink({ uri: "http://localhost:5001" });
 
@@ -17,8 +18,25 @@ const authLink = setContext(() => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+  if (graphQLErrors) {
+    for (let err of graphQLErrors) {
+      switch (err.extensions.code) {
+        case "UNAUTHENTICATED":
+          window.location.href = "/login";
+          // alert
+          // retry the request, returning the new observable
+          return forward(operation);
+      }
+    }
+  }
+  // if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const link = errorLink.concat(authLink);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: link.concat(httpLink),
   cache: new InMemoryCache()
 });
 
